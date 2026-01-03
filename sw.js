97031a24c15bf891df037dd31028e04031ec7ebf
Claudyne Team <dev@claudyne.com>
2026-01-03 17:28:27 +0100

    chore: bump service worker to claudyne-v1.7.7

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Spécialement adapté pour le marché camerounais
  */
 
-const CACHE_NAME = 'claudyne-v1.7.4';
+const CACHE_NAME = 'claudyne-v1.7.7';
 const OFFLINE_URL = '/offline.html';
 
 // Ressources critiques à mettre en cache
